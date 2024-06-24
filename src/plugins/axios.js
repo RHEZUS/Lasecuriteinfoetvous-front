128a1eed77dfee12012be075fc7140c7d9f90524
RHEZUS <ludivinseunda2@gmail.com>
2024-06-24 23:15:37 +0300

    first commit

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 //http://127.0.0.1:8000
 //http://api.lasecuriteinfoetvous.com
 const apiClient = axios.create({
-  baseURL: 'http://api.lasecuriteinfoetvous.com',
+  baseURL: 'https://api.lasecuriteinfoetvous.com',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
